f6e26b8f6c7c18ff9f004bfd37dc72e29440bfa1
Leonardo Caicedo <devleonardocaicedo@gmail.com>
2025-05-27 22:49:58 -0500

    Cuando se obtiene un negocio traer la disponibilidad

--- src/main/java/com/example/Zitapp/Servicios/AvailabilityServicio.java
+++ src/main/java/com/example/Zitapp/Servicios/AvailabilityServicio.java
@@ -1,7 +1,9 @@
 package com.example.Zitapp.Servicios;
 
 import com.example.Zitapp.Modelos.Availability;
+import com.example.Zitapp.Modelos.Business;
 import com.example.Zitapp.Repositorios.AvailabilityRepositorio;
+import com.example.Zitapp.Repositorios.BusinessRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,12 +13,25 @@ import java.util.Optional;
 @Service
 public class AvailabilityServicio {
 
-    @Autowired
-    private AvailabilityRepositorio availabilityRepositorio;
+    private final AvailabilityRepositorio availabilityRepositorio;
+    private final BusinessRepositorio businessRepositorio;
+
+    public AvailabilityServicio(AvailabilityRepositorio availabilityRepositorio, BusinessRepositorio businessRepositorio) {
+        this.availabilityRepositorio = availabilityRepositorio;
+        this.businessRepositorio = businessRepositorio;
+    }
 
     // Crear disponibilidad
-    public Availability crearDisponibilidad(Availability disponibilidad) {
-        return availabilityRepositorio.save(disponibilidad);
+    public Availability crearDisponibilidad(Availability disponibilidad, Long businessId) {
+        Optional<Business> businessOptional = businessRepositorio.findById(businessId);
+
+        if (businessOptional.isPresent()) {
+            Business business = businessOptional.get();
+            disponibilidad.setBusiness(business);
+            return availabilityRepositorio.save(disponibilidad);
+        } else {
+            throw new RuntimeException("Negocio no encontrado con ID: " + businessId);
+        }
     }
 
     // Obtener todas las disponibilidades
@@ -30,14 +45,19 @@ public class AvailabilityServicio {
     }
 
     // Actualizar disponibilidad
-    public Optional<Availability> actualizarDisponibilidad(Integer id, Availability datosActualizados) {
-        return availabilityRepositorio.findById(id).map(disp -> {
-            disp.setIdNegocio(datosActualizados.getIdNegocio());
-            disp.setDia(datosActualizados.getDia());
-            disp.setHoraInicio(datosActualizados.getHoraInicio());
-            disp.setHoraFin(datosActualizados.getHoraFin());
-            return availabilityRepositorio.save(disp);
-        });
+    public Availability actualizarDisponibilidad(Integer id, Availability datosActualizados) {
+        Optional<Availability> availabilityOptional = availabilityRepositorio.findById(id);
+
+        if (availabilityOptional.isPresent()) {
+            Availability availability = availabilityOptional.get();
+            availability.setDia(datosActualizados.getDia());
+            availability.setHoraInicio(datosActualizados.getHoraInicio());
+            availability.setHoraFin((datosActualizados.getHoraFin()));
+
+            return availabilityRepositorio.save(availability);
+        } else {
+            throw new RuntimeException("Disponibilidad no encontrado con ID: " + id);
+        }
     }
 
     // Eliminar disponibilidad
